75b2ab47eec683caeae5f1ff47515433f7b4bb0a
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-19 21:26:48 +0100

    Guarantee that in a new game, X always goes first

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -4,29 +4,19 @@
 import XCTest
 @testable import TicTacToe
 
+class Game {
+    enum Player {
+        case x, o
+    }
+    var currentPlayer: Player = .x
+}
+
 final class TicTacToeTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_playerX_alwaysGoesFirst() {
+        let game = Game()
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(game.currentPlayer, .x)
     }
 
 }
