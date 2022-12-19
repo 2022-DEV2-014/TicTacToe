ec1e49eb9c8abea596ced4334f4f1848ed441551
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-19 21:43:59 +0100

    Guarantee that a new game always has an empty board

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -8,15 +8,25 @@ class Game {
     enum Player {
         case x, o
     }
+
+    // For now let's consider the board as an array of optional players
+    var board: [Player?] = .init(repeating: nil, count: 9)
+
     var currentPlayer: Player = .x
 }
 
 final class TicTacToeTests: XCTestCase {
 
     func test_playerX_alwaysGoesFirst() {
-        let game = Game()
+        let sut = Game()
 
-        XCTAssertEqual(game.currentPlayer, .x)
+        XCTAssertEqual(sut.currentPlayer, .x)
+    }
+
+    func test_board_startsEmpty() {
+        let sut = Game()
+
+        XCTAssertTrue(sut.board.compactMap { $0 }.isEmpty)
     }
 
 }
