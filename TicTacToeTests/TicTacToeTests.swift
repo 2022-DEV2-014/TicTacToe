988a9d279008add005755f8dfa5d3f8c57d0034b
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-19 21:52:48 +0100

    GameState is ready when not started

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -5,9 +5,14 @@ import XCTest
 @testable import TicTacToe
 
 class Game {
+    enum State {
+        case ready
+    }
+
     let board = GameBoard()
 
     var currentPlayer: Player = .x
+    var currentState: State = .ready
 }
 
 enum Player {
@@ -20,5 +25,11 @@ final class GameTests: XCTestCase {
         let sut = Game()
 
         XCTAssertEqual(sut.currentPlayer, .x)
+    }
+
+    func test_gameStateIsReady_whenNotStarted() {
+        let sut = Game()
+
+        XCTAssertEqual(sut.currentState, .ready)
     }
 }
