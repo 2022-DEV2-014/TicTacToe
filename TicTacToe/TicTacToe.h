fd7d7bf519afd520d0b2fb3e41f770fdde0ea35e
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-19 21:06:47 +0100

    Create an empty project that would not leak info about the creator

---
+++ TicTacToe/TicTacToe.h
@@ -0,0 +1,14 @@
+// Created for TicTacToe in 2022
+// Using Swift 5.0
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TicTacToe.
+FOUNDATION_EXPORT double TicTacToeVersionNumber;
+
+//! Project version string for TicTacToe.
+FOUNDATION_EXPORT const unsigned char TicTacToeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TicTacToe/PublicHeader.h>
+
+
