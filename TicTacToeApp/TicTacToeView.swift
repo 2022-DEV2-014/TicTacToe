cc2147700f7003b6e053b0d3f8190b8adcbe1b83
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-21 16:33:43 +0100

    Work together with chatGPT for achieving an animation when a button is tapped

--- TicTacToeApp/TicTacToeView.swift
+++ TicTacToeApp/TicTacToeView.swift
@@ -7,22 +7,42 @@ import SwiftUI
 struct TicTacToeView: View {
     @ObservedObject var game = ObservableGame()
     @State var refreshTrigger = false
+    @State var buttonStates: [Bool] = Array(repeating: false, count: 9)
 
     var body: some View {
         VStack {
             ForEach(0..<3, id: \.self) { row in
                 HStack {
                     ForEach(0..<3, id: \.self) { col in
-                        Button(action: {
-                            do {
-                                try self.game.play(at: (row, col))
-                                refreshTrigger.toggle()
-                            } catch {
-                                print(error)
+                        Circle()
+                            .fill(self.game.game.board.player(at: (row, col)) == "x" ? Color.red : self.game.game.board.player(at: (row, col)) == "o" ? Color.green : Color.blue)
+                            .frame(width: 100, height: 100)
+                            .scaleEffect(buttonStates[row * 3 + col] ? 1.1 : 1)
+                            .onTapGesture {
+                                do {
+                                    try self.game.play(at: (row, col))
+                                    withAnimation(
+                                        Animation
+                                            .easeInOut(duration: 0.2)
+                                            .repeatCount(1, autoreverses: true)
+                                    ) {
+                                        self.buttonStates[row * 3 + col] = true
+                                    }
+
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                                        withAnimation(
+                                            Animation
+                                                .easeInOut(duration: 0.2)
+                                                .repeatCount(1, autoreverses: true)
+                                        ) {
+                                            self.buttonStates[row * 3 + col] = false
+                                        }
+                                    }
+
+                                } catch {
+                                    print(error)
+                                }
                             }
-                        }) {
-                            Text(self.game.game.board.player(at: (row, col)))
-                        }
                     }
                 }
             }
