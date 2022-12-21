2c9ce477c3d5bf936cfc716ee91786cc6be525d3
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-21 15:20:21 +0100

    Add an App target to display and play the game

---
+++ TicTacToeApp/TicTacToeApp.swift
@@ -0,0 +1,13 @@
+// Created for TicTacToe in 2022
+// Using Swift 5.0
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
