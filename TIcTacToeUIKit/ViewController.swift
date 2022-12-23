36ef619b5bb36577395bdd5d826b10fdf390471c
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2022-12-23 17:59:04 +0100

    Create a new project to re-create the UI using UIKit and TDD

---
+++ TIcTacToeUIKit/ViewController.swift
@@ -0,0 +1,15 @@
+// Created for TicTacToe in 2022
+// Using Swift 5.0
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ TIcTacToeUIKitTests/TIcTacToeUIKitTests.swift
@@ -0,0 +1,9 @@
+// Created for TicTacToe in 2022
+// Using Swift 5.0
+
+import XCTest
+@testable import TIcTacToeUIKit
+
+final class TIcTacToeUIKitTests: XCTestCase {
+
+}
