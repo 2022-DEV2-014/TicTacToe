06e4f83eae43291d9d1158e2343683d4ed40e1b0
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2023-01-07 10:39:56 +0100

    Move TicTacToe Game Spy to it's own file

--- TIcTacToeUIKitTests/TicTacToeGame/TicTacToePresenter.swift
+++ TIcTacToeUIKitTests/TicTacToeGame/TicTacToePresenter.swift
@@ -22,52 +22,6 @@ class TicTacToePresenter {
 
 }
 
-class GameSpy: TicTacToe & Resetable {
-
-    var invokedStatusGetter = false
-    var invokedStatusGetterCount = 0
-    var stubbedStatus: String! = ""
-
-    var status: String {
-        invokedStatusGetter = true
-        invokedStatusGetterCount += 1
-        return stubbedStatus
-    }
-
-    var invokedStartedGetter = false
-    var invokedStartedGetterCount = 0
-    var stubbedStarted: Bool! = false
-
-    var started: Bool {
-        invokedStartedGetter = true
-        invokedStartedGetterCount += 1
-        return stubbedStarted
-    }
-
-    var invokedPlay = false
-    var invokedPlayCount = 0
-    var invokedPlayParameters: Int?
-    var invokedPlayParametersList = [Int]()
-    var stubbedPlayError: Error?
-
-    func play(at position: Int) throws {
-        invokedPlay = true
-        invokedPlayCount += 1
-        invokedPlayParameters = position
-        invokedPlayParametersList.append(position)
-        if let error = stubbedPlayError {
-            throw error
-        }
-    }
-
-    var invokedReset = false
-    var invokedResetCount = 0
-    func reset() {
-        invokedReset = true
-        invokedResetCount += 1
-    }
-}
-
 final class TicTacToePresenterTests: XCTestCase {
 
     func test_gameMoves_arePassedAlongToTheGameEngine() {
