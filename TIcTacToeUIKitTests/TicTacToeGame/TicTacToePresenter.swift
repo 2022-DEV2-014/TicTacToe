553d4b96ad324e107753426930fb6c7e40502ff8
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2023-01-07 10:31:45 +0100

    Pass game moves along to the game engine

--- TIcTacToeUIKitTests/TicTacToeGame/TicTacToePresenter.swift
+++ TIcTacToeUIKitTests/TicTacToeGame/TicTacToePresenter.swift
@@ -12,27 +12,72 @@ class TicTacToePresenter {
         self.game = game
     }
 
-}
+    func userPlayedAt(position: Int) {
+        do {
+            try game.play(at: position)
+        } catch {
 
-class GameDummy: Resetable & TicTacToe {
-    var started: Bool = false
-    var status: String = ""
-
-    func play(at position: Int) throws {
-
+        }
     }
 
-    func reset() {
+}
 
+class GameSpy: TicTacToe & Resetable {
+
+    var invokedStatusGetter = false
+    var invokedStatusGetterCount = 0
+    var stubbedStatus: String! = ""
+
+    var status: String {
+        invokedStatusGetter = true
+        invokedStatusGetterCount += 1
+        return stubbedStatus
+    }
+
+    var invokedStartedGetter = false
+    var invokedStartedGetterCount = 0
+    var stubbedStarted: Bool! = false
+
+    var started: Bool {
+        invokedStartedGetter = true
+        invokedStartedGetterCount += 1
+        return stubbedStarted
+    }
+
+    var invokedPlay = false
+    var invokedPlayCount = 0
+    var invokedPlayParameters: Int?
+    var invokedPlayParametersList = [Int]()
+    var stubbedPlayError: Error?
+
+    func play(at position: Int) throws {
+        invokedPlay = true
+        invokedPlayCount += 1
+        invokedPlayParameters = position
+        invokedPlayParametersList.append(position)
+        if let error = stubbedPlayError {
+            throw error
+        }
+    }
+
+    var invokedReset = false
+    var invokedResetCount = 0
+    func reset() {
+        invokedReset = true
+        invokedResetCount += 1
     }
 }
 
 final class TicTacToePresenterTests: XCTestCase {
 
-    func test_init() {
-        let game = GameDummy()
+    func test_gameMoves_arePassedAlongToTheGameEngine() {
+        let game = GameSpy()
         let sut = TicTacToePresenter(game: game)
 
-        XCTAssertNotNil(sut)
+        sut.userPlayedAt(position: 8)
+        sut.userPlayedAt(position: 3)
+        sut.userPlayedAt(position: 5)
+
+        XCTAssertEqual(game.invokedPlayParametersList, [8, 3, 5])
     }
 }
