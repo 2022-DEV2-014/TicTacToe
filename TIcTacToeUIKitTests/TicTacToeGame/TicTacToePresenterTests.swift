5a965ab11b318d57011c5ad280f0d5949b4f8144
2022-DEV2-014 <bromide.pollen_0g@icloud.com>
2023-01-08 09:56:08 +0100

    Add a helper method to create the SUT for the presenter tests

--- TIcTacToeUIKitTests/TicTacToeGame/TicTacToePresenterTests.swift
+++ TIcTacToeUIKitTests/TicTacToeGame/TicTacToePresenterTests.swift
@@ -8,8 +8,7 @@ import XCTest
 final class TicTacToePresenterTests: XCTestCase {
 
     func test_gameMoves_arePassedAlongToTheGameEngine() {
-        let game = GameSpy()
-        let sut = TicTacToePresenter(game: game)
+        let (sut, game, _) = makeSUT()
 
         sut.userPlayedAt(position: 8)
         sut.userPlayedAt(position: 3)
@@ -20,10 +19,7 @@ final class TicTacToePresenterTests: XCTestCase {
     }
 
     func test_afterAMove_anUpdatedBoardIsPassedToTheDisplay() {
-        let game = GameSpy()
-        let display = GameDisplaySpy()
-        let sut = TicTacToePresenter(game: game)
-        sut.display = display
+        let (sut, game, display) = makeSUT()
         game.stubbedBoardRepresentation = [
             "X","O","",
             "" ,"" ,"X",
@@ -40,12 +36,9 @@ final class TicTacToePresenterTests: XCTestCase {
     }
 
     func test_gameMoves_displayAMessageWhenAnErrorIsDetected() {
-        let game = GameSpy()
-        let sut = TicTacToePresenter(game: game)
-        let display = GameDisplaySpy()
-        sut.display = display
-
+        let (sut, game, display) = makeSUT()
         game.stubbedPlayError = NSError(domain: "anyError", code: -1)
+
         sut.userPlayedAt(position: 8)
 
         XCTAssertEqual(display.invokedShowErrorCount, 1)
@@ -54,10 +47,7 @@ final class TicTacToePresenterTests: XCTestCase {
     }
 
     func test_gameReset_isPassedAlongToTheGameEngine() {
-        let game = GameSpy()
-        let sut = TicTacToePresenter(game: game)
-        let display = GameDisplaySpy()
-        sut.display = display
+        let (sut, game, display) = makeSUT()
 
         sut.userRequestedReset()
 
@@ -66,14 +56,10 @@ final class TicTacToePresenterTests: XCTestCase {
     }
 
     func test_afterAGameMove_stateOfTheGameIsRequestedAndUpdated() {
-        let game = GameSpy()
-        let sut = TicTacToePresenter(game: game)
-        let display = GameDisplaySpy()
-        sut.display = display
+        let (sut, game, display) = makeSUT()
 
         game.stubbedStatus = "Random Status"
         sut.userPlayedAt(position: 8)
-
         game.stubbedStatus = "Another Random Status"
         sut.userPlayedAt(position: 3)
 
@@ -81,5 +67,15 @@ final class TicTacToePresenterTests: XCTestCase {
         XCTAssertEqual(display.invokedDisplayStateCount, 2)
         XCTAssertEqual(display.displayStateParametersList, ["Random Status", "Another Random Status"])
     }
-    
+
+    //MARK - Helpers
+
+    private func makeSUT() -> (TicTacToePresenter, GameSpy, GameDisplaySpy) {
+        let game = GameSpy()
+        let sut = TicTacToePresenter(game: game)
+        let display = GameDisplaySpy()
+        sut.display = display
+
+        return (sut, game, display)
+    }
 }
